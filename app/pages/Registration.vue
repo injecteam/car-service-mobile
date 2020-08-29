f6ab6252af27e6abc2badd77f45678f087a73b2c
supervueman <supervueman@gmail.com>
2020-08-29 16:31:59 +0400

    Add axios and test sign up request

--- app/pages/Registration.vue
+++ app/pages/Registration.vue
@@ -2,8 +2,8 @@
 	<Page>
     <ActionBar title="Sign up" />
 		<FlexboxLayout flexDirection="column">
-			<TextField hint="E-mail:" keyboardType="email" />
-			<TextField hint="Password:" />
+			<TextField v-model="email" hint="E-mail:" keyboardType="email" />
+			<TextField v-model="password" hint="Password:" />
 			<Button text="Sign up" @tap="registration" />
 			<Button text="Sign in" @tap="goToSignIn" />
 		</FlexboxLayout>
@@ -12,10 +12,29 @@
 
 <script>
 import App from '../App';
+import axios from 'axios';
 
 export default {
+	data: () => ({
+		email: 'supervueman2@gmail.com',
+		name: 'Rinat',
+		surname: 'Davlikamov',
+		password: 'Password1',
+	}),
+
 	methods: {
-		registrtion() {},
+		registration() {
+			axios.post("http://10.0.2.2:3000/api/users/signup", {
+				email: this.email,
+				name: this.name,
+				surname: this.surname,
+				password: this.password
+			}).then(result => {
+				console.log(result);
+			}, error => {
+				console.log(error);
+			});
+		},
 
 		goToSignIn() {
 			this.$navigateTo(App, {
